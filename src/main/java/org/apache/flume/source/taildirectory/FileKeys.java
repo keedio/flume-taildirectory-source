5f2427b97ccf97bb9d593e2fd4c33978b96ceadf
mvalleavila <mvalleavila@gmail.com>
2015-03-26 21:10:14 +0100

    Tech doubt

--- src/main/java/org/apache/flume/source/taildirectory/FileKeys.java
+++ src/main/java/org/apache/flume/source/taildirectory/FileKeys.java
@@ -5,32 +5,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FileKeys {
 	
-	private static String getOSType() {
-		String osName = System.getProperty("os.name").toLowerCase();
-
-		if (osName.indexOf("win") >= 0)
-			return "win";
-		else if (osName.indexOf("nix") >= 0 || osName.indexOf("nux") >= 0
-				|| osName.indexOf("aix") > 0)
-			return "unix";
-		else if (osName.indexOf("mac") >= 0)
-			return "mac";
-		else if (osName.indexOf("sunos") >= 0)
-			return "solaris";
-		else
-			return "unknown";
-	}
+	private static final Logger LOGGER= LoggerFactory
+			.getLogger(FileKeys.class);
+	
+	private FileKeys(){}
 	
 	public static String getFileKey(Path path){
-		if (getOSType() == "win")
+		if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0)
 			return path.toString();
 		else
 			try{
 				return Files.readAttributes(path, BasicFileAttributes.class)
 						.fileKey().toString();
 			}catch (IOException e){
+				LOGGER.warn(e.getMessage());
+				LOGGER.warn("File {} not found, maby removed/moved",path);
 				return null;
 			}
 	}
